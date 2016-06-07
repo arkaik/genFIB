2666e7211a0f75cce86d2f33cbcbb0cae9d9f264
arkaik <falc@null.net>
2016-06-07 16:23:55 +0200

    id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var main = function(){
     $.getJSON("https://raco.fib.upc.edu/api/assignatures/llista.json",
         function(resp) {
             $.each(resp, function(key, value){
-                console.log(key+":"+value);
+                console.log(key+":"+value[idAssig]);
             });
         }
     );
