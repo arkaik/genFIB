862adfc0a4cc53d4de76dfddd66cf946eadd5533
arkaik <falc@null.net>
2016-06-07 16:36:21 +0200

    idlist

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var main = function(){
     $.getJSON("https://raco.fib.upc.edu/api/assignatures/llista.json",
         function(resp) {
             $.each(resp, function(key, value){
-                console.log(key+":"+value[idAssig]);
+                console.log(key+":"+value.idAssig);
+                var assig_elem = $("<div>"+value.idAssig+"</div>");
+                assig_elem.insertAfter("#assig_list div:last");
             });
         }
     );
