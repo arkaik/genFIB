d63f525e02f8784ae2409a69cb795910e4d414df
arkaik <falc@null.net>
2016-06-07 16:21:11 +0200

    Lulz

---
+++ index.js
@@ -0,0 +1,11 @@
+var main = function(){
+    $.getJSON("https://raco.fib.upc.edu/api/assignatures/llista.json",
+        function(resp) {
+            $.each(resp, function(key, value){
+                console.log(key+":"+value);
+            });
+        }
+    );
+}
+
+$(document).ready(main);
